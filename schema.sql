2a8188b7ca11cacde938ea4b17330e77cd0a8ddc
Junaid Syed <sjunaid626@gmail.com>
2023-07-17 15:54:32 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE IF NOT EXISTS animals (
+  id serial PRIMARY KEY,
+  name varchar(255),
+  date_of_birth date,
+  escape_attempts integer,
+  neutered boolean,
+  weight_kg decimal(10, 2)
 );
