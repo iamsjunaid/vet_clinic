ef5c88d8d14175891daac30b61b00f1d73e4f08f
Junaid Syed <sjunaid626@gmail.com>
2023-07-19 14:09:26 +0530

    insert data into tables

--- data.sql
+++ data.sql
@@ -1,12 +1,14 @@
 /* Populate database with sample data. */
-INSERT INTO animals (
+INSERT INTO
+  animals (
     name,
     date_of_birth,
     escape_attempts,
     neutered,
     weight_kg
   )
-VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
+VALUES
+  ('Agumon', '2020-02-03', 0, true, 10.23),
   ('Gabumon', '2018-11-15', 2, true, 8),
   ('Pikachu', '2021-01-07', 1, false, 15.04),
   ('Devimon', '2017-05-12', 5, true, 11),
@@ -17,3 +19,93 @@ VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
   ('Boarmon', '2005-06-07', 7, true, 20.4),
   ('Blossom', '1998-10-13', 3, true, 17),
   ('Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO
+  owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO
+  species (name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+/* Update species_id column in animals table. */
+UPDATE
+  animals
+SET
+  species_id = (
+    CASE
+      WHEN name LIKE '%mon' THEN (
+        SELECT
+          id
+        FROM
+          species
+        WHERE
+          name = 'Digimon'
+      )
+      ELSE (
+        SELECT
+          id
+        FROM
+          species
+        WHERE
+          name = 'Pokemon'
+      )
+    END
+  );
+
+/* Update owner_id column in animals table. */
+UPDATE
+  animals
+SET
+  owner_id = (
+    CASE
+      WHEN name = 'Agumon' THEN (
+        SELECT
+          id
+        FROM
+          owners
+        WHERE
+          full_name = 'Sam Smith'
+      )
+      WHEN name IN ('Gabumon', 'Pikachu') THEN (
+        SELECT
+          id
+        FROM
+          owners
+        WHERE
+          full_name = 'Jennifer Orwell'
+      )
+      WHEN name IN ('Devimon', 'Plantmon') THEN (
+        SELECT
+          id
+        FROM
+          owners
+        WHERE
+          full_name = 'Bob'
+      )
+      WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (
+        SELECT
+          id
+        FROM
+          owners
+        WHERE
+          full_name = 'Melody Pond'
+      )
+      WHEN name IN ('Angemon', 'Boarmon') THEN (
+        SELECT
+          id
+        FROM
+          owners
+        WHERE
+          full_name = 'Dean Winchester'
+      )
+    END
+  );
